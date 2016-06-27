934b58262d8ca4a6153161ea222262842f1fb4f8
JoniQFZ <JoniQFZ@10.1.1.3>
2016-06-27 14:45:39 +1000

    add logging and change message name

--- NLBookLand/src/main/java/com/nlbookland/controller/BookListController.java
+++ NLBookLand/src/main/java/com/nlbookland/controller/BookListController.java
@@ -2,9 +2,12 @@ package com.nlbookland.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nlbookland.model.Book;
@@ -12,22 +15,29 @@ import com.nlbookland.service.BookService;
 
 @Controller
 public class BookListController {
+	private static final Logger logger = LoggerFactory.getLogger(BookListController.class);
 	String message = "Book List";
 	@Autowired    
 	private BookService bookService;
 	
-	@RequestMapping(value="/booklist")
+	@RequestMapping(method = RequestMethod.GET, value="/booklist")
 	public ModelAndView showBookList(){
-		System.out.println("in booklist controller");
+		//System.out.println("in booklist controller");
+		logger.info("Entered booklist controller - showBookList");
 		ModelAndView mv = new ModelAndView("booklist");
 		
-		List<Book> booklist = bookService.getBookList();
+		List<Book> booklist = getBookList();
 		
 		mv.addObject("booklist", booklist);
 		mv.addObject("book", new Book());
-		mv.addObject("message", message);
+		mv.addObject("booklist_message", message);
 		
-		return mv;
-		
+		return mv;		
+	}
+	
+	private List<Book> getBookList() {
+		List<Book> list = bookService.getBookList();
+		logger.info("Retrived " + list.size() + " books.");
+		return list;
 	}
 }
